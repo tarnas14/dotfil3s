013fefda982bcab864cb126f9d8240121aedc0a8
tarnas14 <tarnas14@gmail.com>
2016-08-22 21:51:17 +0200

    moved silversearcher-ag installation to nvim plugin setup script - it is used by ctrlspace plugin

--- letsgo.sh
+++ letsgo.sh
@@ -17,10 +17,3 @@ else
   echo "nvim already installed"
 fi
 
-if ! command_exists ag ; then
-  echo "installing silversearcher-ag"
-  sudo apt-get install -y silversearcher-ag &> /dev/null
-else
-  echo "ag already installed"
-fi
-
