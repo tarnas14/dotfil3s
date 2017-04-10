9c21ca31e4c0ae35cf2dbf179b753b7905e9cb69
tarnas14 <tarnas14@gmail.com>
2017-04-10 22:56:16 +0200

    added tlp installation

--- letsgo.sh
+++ letsgo.sh
@@ -18,3 +18,11 @@ else
   echo "nvim already installed"
 fi
 
+if ! command_exists tlp ; then
+  echo "installing tlp"
+  sudo add-apt-repository ppa:linrunner/tlp
+  sudo apt-get update
+  sudo apt-get install tlp tlp-rdw smartmontools ethtool
+else
+  echo "tlp already installed"
+fi
