54f9ccf487b17b32cc600188a4340014394f4162
tarnas14 <tarnas14@gmail.com>
2017-04-12 23:24:31 +0200

    adding --assume-yes to everything in `letsgo`, added curl install

--- letsgo.sh
+++ letsgo.sh
@@ -8,7 +8,7 @@ if ! command_exists tlp ; then
   echo "installing tlp"
   sudo add-apt-repository ppa:linrunner/tlp
   sudo apt-get update
-  sudo apt-get install tlp tlp-rdw smartmontools ethtool
+  sudo apt-get -y install tlp tlp-rdw smartmontools ethtool
 else
   echo "tlp already installed"
 fi
@@ -20,8 +20,8 @@ fi
 
 if ! command_exists tmux ; then
   echo "installing tmux"
-  sudo apt-get install libevent-dev
-  sudo apt-get install ncurses-dev
+  sudo apt-get -y install libevent-dev
+  sudo apt-get -y install ncurses-dev
   cd ~/Downloads
   wget https://github.com/tmux/tmux/releases/download/2.3/tmux-2.3.tar.gz
   tar -zxf tmux-2.3.tar.gz -C ~/apps
@@ -35,24 +35,28 @@ else
   echo "tmux already installed"
 fi
 
+if ! command_exists curl ; then
+  echo "installing curl"
+  sudo apt-get -y install curl
+fi
+
 if [ ! -d "/home/tarnas/.nvm" ]; then
   echo "installing nvm"
   curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
-  nvm install node
 else
   echo "nvm already there"
 fi
 
 if ! command_exists nvim ; then
   echo "installing nvim"
-  sudo apt-get install -y software-properties-common &> /dev/null
-  sudo apt-get install -y python-dev python-pip python3-dev python3-pip &> /dev/null
+  sudo apt-get install -y software-properties-common
+  sudo apt-get install -y python-dev python-pip python3-dev python3-pip
   sudo python2 -m pip install neovim
   sudo python3 -m pip install neovim
   sudo add-apt-repository ppa:neovim-ppa/unstable
   sudo apt-get update
-  sudo apt-get install xclip
-  sudo apt-get install -y neovim
+  sudo apt-get -y install xclip
+  sudo apt-get -y install neovim
 else
   echo "nvim already installed"
 fi
