06f14756f47c69ea2041d52d4f44251aaa5a90b2
tarnas14 <tarnas14@gmail.com>
2018-02-15 19:09:11 +0100

    added port publishing 3000-3005 to the startEnv

--- startEnv.sh
+++ startEnv.sh
@@ -1,4 +1,5 @@
 docker run -it --rm \
+  -p 3000:3000 -p 3001:3001 -p 3002:3002 -p 3003:3003 -p 3004:3004 \
   --mount type=bind,src=/home/tarnas/dotfil3s,dst=/home/tarnasenv/dotfil3s \
   --mount type=bind,src=/home/tarnas/projects,dst=/home/tarnasenv/projects \
   --mount type=bind,src=/home/tarnas/Dropbox/todos,dst=/home/tarnasenv/todos \
