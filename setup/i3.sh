c97e5b9d0e4f6403dc04f8b90e18a47a588dc791
tarnas14 <tarnas14@gmail.com>
2018-04-16 11:59:15 +0200

    setup audio playback control with playerctl

--- setup/i3.sh
+++ setup/i3.sh
@@ -3,3 +3,9 @@
 # dependencies
 sudo apt-get install scrot -y
 sudo apt-get install rofi -y
+
+wget -0 package.deb "https://github.com/acrisci/playerctl/releases/download/v0.6.0/playerctl-0.6.0_amd64.deb"
+sudo dpkg -i package.deb
+rm package.deb
+
+sudo apt-get install -f
