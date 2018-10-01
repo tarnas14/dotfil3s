734f93de5bedbe9de0ecf2b8ba0d6a82e2f6efd1
tarnas-softserve <mtar@softserveinc.com>
2018-10-01 14:26:57 +0200

    wget -O instead of wget -0

--- setup/i3.sh
+++ setup/i3.sh
@@ -4,7 +4,7 @@
 sudo apt-get install scrot -y
 sudo apt-get install rofi -y
 
-wget -0 package.deb "https://github.com/acrisci/playerctl/releases/download/v0.6.0/playerctl-0.6.0_amd64.deb"
+wget -O package.deb "https://github.com/acrisci/playerctl/releases/download/v0.6.0/playerctl-0.6.0_amd64.deb"
 sudo dpkg -i package.deb
 rm package.deb
 
