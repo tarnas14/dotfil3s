56474ccf4c12099ae7651f4a4ba5b7e793f75e52
tarnas-softserve <mtar@softserveinc.com>
2018-10-02 08:06:59 +0200

    added instructions how to setup i3

--- setup/i3.sh
+++ setup/i3.sh
@@ -3,6 +3,13 @@
 # dependencies
 sudo apt-get install scrot -y
 sudo apt-get install rofi -y
+sudo apt-get install help2man -y
+
+# light
+echo "to install light:"
+echo "git submodule update"
+echo "cd i3/light"
+echo "make && sudo make install"
 
 wget -O package.deb "https://github.com/acrisci/playerctl/releases/download/v0.6.0/playerctl-0.6.0_amd64.deb"
 sudo dpkg -i package.deb
