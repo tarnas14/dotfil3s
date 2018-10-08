acd4ce033c5a0cb2f54ac3a3a0cf7b693483d5de
tarnas-softserve <mtarn@softserveinc.com>
2018-10-08 08:53:30 +0200

    fixed center on polybar, added deprecated dependency to install script

--- polybar/install-polybar-deps.sh
+++ polybar/install-polybar-deps.sh
@@ -7,3 +7,6 @@ sudo apt-get install -y python-xcbgen xcb-proto
 sudo apt-get install -y libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev
 
 sudo apt-get install -y libpulse-dev i3-wm libjsoncpp-dev libnl-3-dev
+
+# deprecated
+# sudo apt-get install -y libiw-dev
