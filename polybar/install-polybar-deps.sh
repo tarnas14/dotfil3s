10708c08e330e73068f2227300a1688eeb63c5cd
tarnas-softserve <mtarn@softserveinc.com>
2018-10-08 08:40:28 +0200

    added script installing polybar dependencies before installation

---
+++ polybar/install-polybar-deps.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get install -y libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite--dev
+
+sudo apt-get install -y python-xcbgen xcb-proto
+
+sudo apt-get install -y libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev
+
+sudo apt-get install -y libpulse-dev i3-wm libjsoncpp-dev libnl-3-dev
