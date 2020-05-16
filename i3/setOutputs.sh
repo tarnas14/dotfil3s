6c29bd991ab72f6a71cada75e34256a26bc9c547
tarnas14 <tarnas14@gmail.com>
2020-05-16 17:17:53 +0200

    revamp monitor handling - added monitor disconnect

--- i3/setOutputs.sh
+++ i3/setOutputs.sh
@@ -7,18 +7,22 @@ MODE=$3
 
 function connect(){
   if [ $MODE = "right" ]; then
-    xrandr --output $MONITOR_OUTPUT --right-of $LAPTOP_OUTPUT --auto
+    xrandr --output $MONITOR_OUTPUT --auto --right-of $LAPTOP_OUTPUT --auto
   elif [ $MODE = "left" ]; then
-    xrandr --output $MONITOR_OUTPUT --left-of $LAPTOP_OUTPUT --auto
+    xrandr --output $MONITOR_OUTPUT --auto --left-of $LAPTOP_OUTPUT --auto
   elif [ $MODE = "mirror" ]; then
-    xrandr --output $MONITOR_OUTPUT --same-as $LAPTOP_OUTPUT --auto
+    xrandr --output $MONITOR_OUTPUT --auto --same-as $LAPTOP_OUTPUT --auto
+  elif [ $MODE = "onlyMonitor" ]; then
+    xrandr --output $MONITOR_OUTPUT --auto --output $LAPTOP_OUTPUT --off
+  elif [ $MODE = "above" ]; then
+    xrandr --output $MONITOR_OUTPUT --auto --above $LAPTOP_OUTPUT --auto
   else
-    xrandr --output $MONITOR_OUTPUT --above $LAPTOP_OUTPUT --auto
+    xrandr --output $MONITOR_OUTPUT --off --output $LAPTOP_OUTPUT --auto
   fi
 }
 
 function disconnect(){
-  xrandr --output $MONITOR_OUTPUT --off
+  xrandr --output $MONITOR_OUTPUT --off --output $LAPTOP_OUTPUT --auto
 }
 
 xrandr | grep "$MONITOR_OUTPUT connected" &> /dev/null && connect || disconnect
