6e230259beb491fc03e8b6dd73335187db874fe1
tarnas14 <tarnas14@gmail.com>
2020-12-12 19:42:56 +0100

    removed setup script for tlp, updated omzsh and nvim setup scripts

--- setup/nvim.sh
+++ setup/nvim.sh
@@ -1,27 +1,11 @@
 #!/bin/bash
 
-. ./utils.sh
+sudo pacman -S the_silver_searcher --needed --noconfirm
+yay -S --noconfirm vim-plug
+sudo pacman -S neovim --needed --noconfirm
+sudo pacman -S python-pynvim --needed --noconfirm
 
-if ! command_exists nvim ; then
-  # for you complete me
-  sudo apt-get install -y silversearcher-ag
-  sudo apt-get install -y cmake
+rm -rf ~/.config/nvim
+ln -s ~/dotfil3s/nvim ~/.config/nvim
 
-  sudo apt-get install -y python-dev python-pip python3-dev python3-pip \
-    && python2 -m pip install neovim \
-    && python3 -m pip install neovim
-  curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
-  chmod u+x nvim.appimage
-  mv ./nvim.appimage ~/apps/nvim
-  sudo ln -s ~/apps/nvim /usr/local/bin
-
-  sudo apt-get install -y curl
-  sudo curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-  ln -s "$dotfil3s_root"nvim ~/.config/
-
-  ~/apps/nvim -c "PlugInstall|q|q"
-else
-  echo "nvim already installed"
-fi
+nvim -c "PlugInstall|q|q"
--- setup/omzsh.sh
+++ setup/omzsh.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-if [ $SHELL == "/usr/bin/zsh" ]; then
-  echo "zsh already installed, probably oh my zsh is already there"
-else
-  sudo apt-get -y install zsh
-  wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh
-  #sed -i.tmp 's:env zsh::g' install.sh
-  #sed -i.tmp 's:chsh -s .*$::g' install.sh
-  sh install.sh
-  rm install.sh
-fi
+yay -S --noconfirm oh-my-zsh-git
+echo 'n' | /usr/share/oh-my-zsh/tools/install.sh
+
+rm -f ~/.zshrc
+ln -s ~/dotfil3s/.zshrc ~/.zshrc
