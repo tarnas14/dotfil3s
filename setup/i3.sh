e14401e2db5d4ddd9da426260d3dedb0eb35c0b8
tarnas14 <tarnas14@gmail.com>
2020-12-13 02:35:57 +0100

    added i3lock.service to automatically run i3lock on suspend

--- setup/i3.sh
+++ setup/i3.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 # dependencies
+sudo pacman -S --needed --noconfirm i3
 sudo pacman -S --needed --noconfirm scrot
 sudo pacman -S --needed --noconfirm rofi
 sudo pacman -S --needed --noconfirm dmenu
 sudo pacman -S --needed --noconfirm feh
 sudo pacman -S --needed --noconfirm playerctl
 sudo pacman -S --needed --noconfirm polybar
-#!/bin/bash
-
-. ./utils.sh
 
 rm -f ~/.onedark-theme.rasi
 ln -s ~/dotfil3s/i3/onedark-theme.rasi ~/onedark-theme.rasi
@@ -17,3 +15,6 @@ rm -rf ~/.config/i3
 ln -s ~/dotfil3s/i3 ~/.config/i3
 rm -rf ~/.config/polybar
 ln -s ~/dotfil3s/polybar ~/.config/polybar
+
+sudo cp ../i3lock.service /etc/systemd/system/i3lock.service
+sudo systemctl enable i3lock.service
