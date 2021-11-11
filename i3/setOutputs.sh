d512873e928b8e3e17405ce3af160af0710683ea
tarnas14 <tarnas14@gmail.com>
2021-11-11 22:42:03 +0100

    small fix to monitor management

--- i3/setOutputs.sh
+++ i3/setOutputs.sh
@@ -22,28 +22,39 @@ function onlyOne(){
 
 function connect(){
   if [ $MODE = "right" ]; then
+    echo "right"
     xrandr --output $MONITOR_OUTPUT --auto --right-of $LAPTOP_OUTPUT --auto
-    alwaysTwo
+    # alwaysTwo
   elif [ $MODE = "left" ]; then
+    echot "left"
     xrandr --output $MONITOR_OUTPUT --auto --left-of $LAPTOP_OUTPUT --auto
-    alwaysTwo
+    # alwaysTwo
   elif [ $MODE = "mirror" ]; then
+    echo "mirror"
     xrandr --output $MONITOR_OUTPUT --auto --same-as $LAPTOP_OUTPUT --auto
-    alwaysTwo
+    # alwaysTwo
   elif [ $MODE = "onlyMonitor" ]; then
+    echo "only"
     xrandr --output $MONITOR_OUTPUT --auto --output $LAPTOP_OUTPUT --off
   elif [ $MODE = "above" ]; then
+    echo "above"
     xrandr --output $MONITOR_OUTPUT --auto --above $LAPTOP_OUTPUT --auto
-    alwaysTwo
+    # alwaysTwo
   else
+    echo "else"
     xrandr --output $MONITOR_OUTPUT --off --output $LAPTOP_OUTPUT --auto
-    onlyOne
+    # onlyOne
   fi
 }
 
 function disconnect(){
+  echo "disconnect"
   xrandr --output $MONITOR_OUTPUT --off --output $LAPTOP_OUTPUT --auto
-  onlyOne
+  # onlyOne
 }
 
-xrandr | grep "$MONITOR_OUTPUT connected" &> /dev/null && connect || disconnect
+if [ "$(xrandr | grep -q '$MONITOR_OUTPUT connected 1920x1080'))" -ge 0 ]; then
+  disconnect
+else
+  connect
+fi
