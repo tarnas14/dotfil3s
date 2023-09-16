cbe8d5d67284402774907b03ebf0ba2c600df88a
tarnas14 <tarnas14@gmail.com>
2023-09-16 20:42:05 +0200

    bunch of changes related to csharp lsp

--- nvim/init.lua
+++ nvim/init.lua
@@ -54,9 +54,9 @@ require("lazy").setup({
 	},
 	{
 		"neovim/nvim-lspconfig",
-		build = "npm install -g dockerfile-language-server-nodejs @microsoft/compose-language-service",
+		build = "npm install -g dockerfile-language-server-nodejs @microsoft/compose-language-service typescript-language-server typescript",
 	},
-	-- possibly misconfigured?
+	-- this is configured correctly, but most language servers don't send it :(
 	{
 		"j-hui/fidget.nvim",
 		tag = "legacy",
@@ -146,7 +146,7 @@ require("lazy").setup({
 		build = ":TSUpdate",
 		config = function()
 			require("nvim-treesitter.configs").setup({
-				ensure_installed = { "elixir", "heex", "eex", "yaml" },
+				ensure_installed = { "elixir", "heex", "eex", "yaml", "c_sharp", "dockerfile", "javascript" },
 				highlight = {
 					enable = true,
 					additional_vim_regex_highlighting = false,
@@ -175,7 +175,6 @@ require("lazy").setup({
 		"nvim-lualine/lualine.nvim",
 		dependencies = {
 			"nvim-tree/nvim-web-devicons",
-			"arkav/lualine-lsp-progress",
 			"nvim-lua/lsp-status.nvim",
 		},
 	},
@@ -244,10 +243,10 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		-- Buffer local mappings.
 		-- See `:help vim.lsp.*` for documentation on any of the below functions
 		local opts = { buffer = ev.buf }
-		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "<leader>d", vim.lsp.buf.declaration, opts)
+		vim.keymap.set("n", "<leader>g", vim.lsp.buf.definition, opts)
 		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+		vim.keymap.set("n", "<leader>i", vim.lsp.buf.implementation, opts)
 		vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
 		vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
 		vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
@@ -331,13 +330,52 @@ require("lspconfig").dockerls.setup({
 	capabilities = capabilities,
 })
 
+require("lspconfig").omnisharp.setup({
+	-- cmd = { "dotnet", "/path/to/omnisharp/OmniSharp.dll" },
+
+	-- Enables support for reading code style, naming convention and analyzer
+	-- settings from .editorconfig.
+	enable_editorconfig_support = true,
+
+	-- If true, MSBuild project system will only load projects for files that
+	-- were opened in the editor. This setting is useful for big C# codebases
+	-- and allows for faster initialization of code navigation features only
+	-- for projects that are relevant to code that is being edited. With this
+	-- setting enabled OmniSharp may load fewer projects and may thus display
+	-- incomplete reference lists for symbols.
+	enable_ms_build_load_projects_on_demand = false,
+
+	-- Enables support for roslyn analyzers, code fixes and rulesets.
+	enable_roslyn_analyzers = false,
+
+	-- Specifies whether 'using' directives should be grouped and sorted during
+	-- document formatting.
+	organize_imports_on_format = false,
+
+	-- Enables support for showing unimported types and unimported extension
+	-- methods in completion lists. When committed, the appropriate using
+	-- directive will be added at the top of the current file. This option can
+	-- have a negative impact on initial completion responsiveness,
+	-- particularly for the first few completion sessions after opening a
+	-- solution.
+	enable_import_completion = false,
+
+	-- Specifies whether to include preview versions of the .NET SDK when
+	-- determining which version to use for project loading.
+	sdk_include_prereleases = true,
+
+	-- Only run analyzers against open files when 'enableRoslynAnalyzers' is
+	-- true
+	analyze_open_documents_only = false,
+})
+
 require("lspconfig").docker_compose_language_service.setup({})
 -- set filetype for docker-compose files to load this lsp
 vim.filetype.add({
-  filename = {
-    ['docker-compose.yml'] = 'yaml.docker-compose',
-    ['docker-compose.yaml'] = 'yaml.docker-compose',
-  },
+	filename = {
+		["docker-compose.yml"] = "yaml.docker-compose",
+		["docker-compose.yaml"] = "yaml.docker-compose",
+	},
 })
 
 local elixir_capabilities = require("cmp_nvim_lsp").default_capabilities()
@@ -351,7 +389,7 @@ require("lualine").setup({
 	sections = {
 		lualine_a = { "mode" },
 		lualine_b = { "diagnostics" },
-		lualine_c = { "lsp_progress" },
+		lualine_c = { },
 		lualine_x = { "encoding", "filetype", "require'lsp-status'.status()" },
 		lualine_y = {},
 		lualine_z = { "location" },
